36b2ee7263f809f4eb6f4eb7f2980564abafb63c
h-parvathy <76555469+h-parvathy@users.noreply.github.com>
2021-04-12 22:11:54 -0400

    Exception added

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,20 +3,24 @@ import java.io.IOException;
 
 public class MainClass {
     public static void main(String[] args) throws IOException {
-        JFrame input;
-        input=new JFrame();
+        try{
+            JFrame input;
+            input=new JFrame();
 
-        int id = Integer.parseInt(JOptionPane.showInputDialog(input, "Enter Employee ID:"));
-        String name = JOptionPane.showInputDialog(input, "Enter Employee Username:");
-        double sales = Double.parseDouble(JOptionPane.showInputDialog(input, "Enter sales made this month"));
-        double advance = Double.parseDouble(JOptionPane.showInputDialog(input, "Enter advance taken this month:"));
-        String month = JOptionPane.showInputDialog(input, "Enter the month:");
+            int id = Integer.parseInt(JOptionPane.showInputDialog(input, "Enter Employee ID:"));
+            String name = JOptionPane.showInputDialog(input, "Enter Employee Username:");
+            double sales = Double.parseDouble(JOptionPane.showInputDialog(input, "Enter sales made this month"));
+            double advance = Double.parseDouble(JOptionPane.showInputDialog(input, "Enter advance taken this month:"));
+            String month = JOptionPane.showInputDialog(input, "Enter the month:");
 
 
-        CommissionCalculator calculator = new CommissionCalculator(id,name,sales,advance,month);
-        calculator.calcPay();
-        calculator.empDetails();
-        calculator.saveToFile();
-        calculator.printYourName();
+            CommissionCalculator calculator = new CommissionCalculator(id,name,sales,advance,month);
+            calculator.calcPay();
+            calculator.empDetails();
+            calculator.saveToFile();
+            calculator.printYourName();
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
